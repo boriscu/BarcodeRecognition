f66b0b6387689c3226617cad7845228fb30c04bc
Boris <borisculjak@gmail.com>
2024-05-28 12:23:10 +0200

    Added working barcode recognition

--- recognizeBarcode.m
+++ recognizeBarcode.m
@@ -1,39 +1,110 @@
-% Barcode recognition function
 function barcodeData = recognizeBarcode(img)
-    binaryImg = imbinarize(img);
+    if(size(img,3) == 3)
+        img = rgb2gray(img);
+    end
+    img = detector(img);
+    if(size(img,3) == 3)
+        img = rgb2gray(img);
+    end
+
+    [~,b] = sort(-sum((1-img')));
+    sel = round(mean(b(1:50)));
+
+    range = 10;
+    band = img(sel-range:sel+range,:);
+    inp =round(sum(band)./(2*range));
+
+    idx = find(diff([inp(1)-1, inp]));
+    barCode = [inp(idx); diff([idx,numel(inp)+1])];
+    barCode(1,:) = (barCode(1,:) ~= 1); % Fliping 0's and `'s
+
+    len = size(barCode,2);
+    s_idx = 1;
+    for i = 1:len-2
+       if isequal(barCode(1,[i,i+1,i+2]),[1,0,1])
+           s_idx = i+3;
+           break;
+       end
+    end
+
+    % map => mapping of the digit encoding
+    %     => [left_odd,left_even, right,relation_to_digit_1]
+    map = [0:9;
+        "0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011";
+        "0100111", "0110011", "0011011", "0100001", "0011101", "0111001", "0000101", "0010001", "0001001", "0010111";
+        "1110010", "1100110", "1101100", "1000010", "1011100" ,"1001110", "1010000", "1000100", "1001000", "1110100";
+        "OOOOOOEEEEEE", "OOEOEEEEEEEE", "OOEEOEEEEEEE", "OOEEEOEEEEEE", "OEOOEEEEEEEE", "OEEOOEEEEEEE", "OEEEOOEEEEEE", "OEOEOEEEEEEE", "OEOEEOEEEEEE", "OEEOEOEEEEEE";];
+    left = [3 2 1 1; 2 2 2 1; 2 1 2 2; 1 4 1 1;1 1 3 2;1 2 3 1; 1 1 1 4; 1 3 1 2; 1 2 1 3; 3 1 1 2; 1 1 2 3; 1 2 2 2; 2 2 1 2; 1 1 4 1; 2 3 1 1; 1 3 2 1; 4 1 1 1; 2 1 3 1;3 1 2 1; 2 1 1 3];
+    left_p = ['O','O','O','O','O','O','O','O','O','O','E','E','E','E','E','E','E','E','E','E'];
+    left_m = [0:9, 0:9];
+    right = [3 2 1 1; 2 2 2 1; 2 1 2 2; 1 4 1 1;1 1 3 2;1 2 3 1; 1 1 1 4; 1 3 1 2; 1 2 1 3; 3 1 1 2;];
+    right_m = 0:9;
+
+    first = barCode(:,s_idx:s_idx+6*4-1);
+    width = zeros(1,size(first(1,:),2));
+    for i = 1:4:size(first(1,:),2)
+        tmp = [i,i+1,i+2,i+3];
+        x = sum(first(2,tmp))/7;
+        width(tmp) = x;
+    end
+    first(2,:) = first(2,:)./width;
+    second = barCode(:,s_idx+6*4+5:s_idx+2*6*4+5-1);
+    for i = 1:4:size(first(1,:),2)
+        tmp = [i,i+1,i+2,i+3];
+        x = sum(second(2,tmp))/7;
+        width(tmp) = x;
+    end
+    second(2,:) = second(2,:)./width;
+
+    str = "";
+    str = strcat(str,repmat(int2str(first(1,1)),1,round(first(2,1))));
+    str = strcat(str,repmat(int2str(first(1,2)),1,round(first(2,2))));
+    str = strcat(str,repmat(int2str(first(1,3)),1,round(first(2,3))));
+    str = strcat(str,repmat(int2str(first(1,4)),1,round(first(2,4))));
     
-    % Assuming the barcode is horizontal and spans the entire width
-    % Take a central row to analyze
-    middleRow = binaryImg(round(size(binaryImg, 1) / 2), :);
-    
-    % Detect transitions from white to black and vice versa
-    transitions = diff([0, middleRow, 0] ~= 0);
-    transitionIndices = find(transitions);
+    % The first digit from the left group is always encoded with odd parity
+    if isempty(map(2,ismember(map(2,:),str)))
+       temp = first;
+       first = second(:,size(second,2):-1:1);
+       second = temp(:,size(temp,2):-1:1);
+    end
 
-    % Measure the width of each bar/space
-    widths = diff(transitionIndices);
+    code = [first,second]; % Bar code in the right order
 
-    % Display the image
-    imshow(img); 
-    hold on;
 
-    % Displaying detected transitions on the image
-    currentX = 1; % Initial position on the x-axis
-    lineColor = [1 0 0]; % Start with red
-    for i = 1:length(widths)
-        % Draw a horizontal line at the middle of the barcode
-        line([currentX, currentX + widths(i)], [round(size(img, 1) / 2), round(size(img, 1) / 2)], 'Color', lineColor, 'LineWidth', 2);
-        currentX = currentX + widths(i); % Move to the next bar/space
+    barcodeData = ""; % Decoded Text
+    par = ""; % Holds the parity of each digit
 
-        % Alternate line color between red and blue
-        if isequal(lineColor, [1 0 0])  % Red
-            lineColor = [0 0 1]; % Change to blue
+    for i=1:4:size(code,2)
+        str = "";
+        str = strcat(str,repmat(int2str(code(1,i)),1,round(code(2,i))));
+        str = strcat(str,repmat(int2str(code(1,i+1)),1,round(code(2,i+1))));
+        str = strcat(str,repmat(int2str(code(1,i+2)),1,round(code(2,i+2))));
+        str = strcat(str,repmat(int2str(code(1,i+3)),1,round(code(2,i+3))));
+        
+
+        if ~isempty(map(1,ismember(map(2,:),str)))
+            barcodeData = strcat(barcodeData,map(1,ismember(map(2,:),str)));
+            par = strcat(par,"O");
+        elseif ~isempty(map(1,ismember(map(3,:),str)))
+            barcodeData = strcat(barcodeData,map(1,ismember(map(3,:),str)));
+            par = strcat(par,"E");
+        elseif ~isempty(map(1,ismember(map(4,:),str)))
+            barcodeData = strcat(barcodeData,map(1,ismember(map(4,:),str)));
+            par = strcat(par,"E");
         else
-            lineColor = [1 0 0]; % Change back to red
+            if code(1,i) == 0
+                idx = knnsearch(left,code(2,[i,i+1,i+2,i+3]));
+                barcodeData = strcat(barcodeData,int2str(left_m(idx)));
+                par = strcat(par,left_p(idx));
+            else
+                idx = knnsearch(right,code(2,[i,i+1,i+2,i+3]));
+                barcodeData = strcat(barcodeData,int2str(right_m(idx)));
+                par = strcat(par,'E');
+            end
         end
     end
-    
-    hold off;
-    barcodeData = '860037004075'; 
 
-    
+    barcodeData = strcat(map(1,ismember(map(5,:),par)),barcodeData);
+    disp(barcodeData)
+end
